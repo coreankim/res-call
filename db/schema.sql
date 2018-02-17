dcb88a25d8ccf6d92429527d7f249d03972f34bf
coreankim@gmail.com <coreankim@gmail.com>
2018-02-17 16:40:28 -0500

    This is third version

--- db/schema.sql
+++ db/schema.sql
@@ -4,22 +4,25 @@ SET time_zone = "+00:00";
 DROP DATABASE IF EXISTS resCallDB;
 
 CREATE DATABASE resCallDB;
+
 USE resCallDB;
 
 CREATE TABLE scenarios (
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   body_part VARCHAR(100),
   body_part_specific VARCHAR(200),
   plan TEXT,
+  createdAt TIMESTAMP NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE patients (
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+ id INT AUTO_INCREMENT NOT NULL,
   patient_name VARCHAR(100),
   MRN VARCHAR(100),
   HPI TEXT,
   injury VARCHAR(200),
   plan TEXT,
+  createdAt TIMESTAMP NOT NULL,
   PRIMARY KEY (id)
 );
